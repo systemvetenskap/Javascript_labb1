24bb59709ad63fb9212a3cbfd5728e80c8fde883
Erik Öberg <erik.oberg@miun.se>
2022-10-12 14:28:41 +0200

    Remove console.log

--- src/labb.js
+++ src/labb.js
@@ -1,41 +1,31 @@
-
 // modifiera sum() tills testet blir godkänt!
 function sum(a, b) {
-
-    console.log("new")
-    console.log("hej")
+  console.log("funct sum");
 }
 
-function myOwnMultiplyFunction() {
-console.log("test")
-}
+function myOwnMultiplyFunction() {}
 
 // denna måste exporteras längst ned i denna fil! annars kan labb.test.js inte importera den.
-function isBelowZero() {
-
-}
-
+function isBelowZero() {}
 
 // fortsätt skapa alla lösningar som beskrivs i labb.test.js och exportera dem!
 
-
 //etc...
 
-
 // https://eloquentjavascript.net/10_modules.html#h_N33QHgUxbG how require and modules work in JavaScript
-module.exports.sum = sum
-module.exports.multiply = myOwnMultiplyFunction
-module.exports.isBelowZero = undefined
-module.exports.round = undefined
-module.exports.addingUp = undefined
-module.exports.findMinMax = undefined
-module.exports.afterXmasEve2020 = undefined
-module.exports.sortByStringLength = undefined
-module.exports.charCounter = undefined
-module.exports.numbersOnly = undefined
-module.exports.sortNumbers = undefined
-module.exports.personFactory = undefined
-module.exports.doublePrice = undefined
-module.exports.and = undefined
-module.exports.removeLeadingTrailing = undefined
-module.exports.getKeysAndValues = undefined
+module.exports.sum = sum;
+module.exports.multiply = myOwnMultiplyFunction;
+module.exports.isBelowZero = undefined;
+module.exports.round = undefined;
+module.exports.addingUp = undefined;
+module.exports.findMinMax = undefined;
+module.exports.afterXmasEve2020 = undefined;
+module.exports.sortByStringLength = undefined;
+module.exports.charCounter = undefined;
+module.exports.numbersOnly = undefined;
+module.exports.sortNumbers = undefined;
+module.exports.personFactory = undefined;
+module.exports.doublePrice = undefined;
+module.exports.and = undefined;
+module.exports.removeLeadingTrailing = undefined;
+module.exports.getKeysAndValues = undefined;
